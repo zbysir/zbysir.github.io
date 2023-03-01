8b143d95d8763bc36142471971ed5ac5c6dbed18
zhangliang <zbysir@qq.com>
2023-03-01 11:12:39 +0800

    一百个为什么 添加 tag 交互

--- contents/2023/Tags.tsx
+++ contents/2023/Tags.tsx
@@ -5,7 +5,7 @@ export default ({tags}: { tags: string|string[] }) => {
     }else{
         ts = tags
     }
-    return <div className={'flex flex-nowrap space-x-2 !-mt-6 opacity-50'}>
-        {ts.map(t => <div>#{t}</div>)}
+    return <div className={'flex flex-nowrap space-x-2 !-mt-6'}>
+        {ts.map(t => <div className="opacity-50">#{t}</div>)}
     </div>
 }
