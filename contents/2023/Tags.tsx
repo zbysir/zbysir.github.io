c5492a5c5961ef9dc08a6cd05c342fecde87b443
zhangliang <zbysir@qq.com>
2023-03-04 20:54:30 +0800

    优化一百个为什么 tag

--- contents/2023/Tags.tsx
+++ contents/2023/Tags.tsx
@@ -1,10 +1,12 @@
 export default ({tags}: { tags: string|string[] }) => {
     let ts = []
-    if (typeof tags=='string'){
+    // split string to array
+    if (typeof tags == 'string') {
         ts = tags.split(',')
-    }else{
+    } else {
         ts = tags
     }
+
     return <div className={'flex flex-nowrap space-x-2 !-mt-6'}>
         {ts.map(t => <div className="opacity-50">#{t}</div>)}
     </div>
